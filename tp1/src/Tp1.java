bf347c1101d32ddf815ef9708d60af7a2724a845
Maxime Chevignard <maxime@chevignard.fr>
2017-10-18 22:43:59 +0200

    super ça marche sur le cluster

--- tp1/src/Tp1.java
+++ tp1/src/Tp1.java
@@ -1,7 +1,7 @@
 import org.apache.hadoop.mapreduce.*;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.LongWritable;
+
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
@@ -10,31 +10,36 @@ import org.apache.commons.lang.StringUtils;
 import java.io.IOException;
 import java.util.*;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.LongWritable;
 
 public class Tp1{
 public static class PivotMap extends Mapper<LongWritable, Text, LongWritable, Text> {
     @Override
-    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        long column = 0;
-        long somethingLikeRow = key.get();
+    protected void map(LongWritable key, Text text, Context context) throws IOException, InterruptedException {
+        long colonne = 0;
+        long ligne = key.get();
         /*réglage du splitter en semi-column*/
-        for (String num : value.toString().split(";")) {
-            context.write(new LongWritable(column), new Text(somethingLikeRow + "\t" + num));
-            ++column;
+        for (String num : text.toString().split(";")) {
+        	/*pour chaque ligne récupérée on créé alors une nouvelle colonne*/
+            context.write(new LongWritable(colonne), new Text(ligne + ";" + num));
+            colonne++;
         }
     }
 }
 
 public static class PivotReduce extends Reducer<LongWritable, Text, LongWritable, Text> {
     @Override
-    protected void reduce(LongWritable key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-        TreeMap<Long, String> row = new TreeMap<Long, String>();
-        for (Text text : values) {
-            String[] parts = text.toString().split("\t");
-            row.put(Long.valueOf(parts[0]), parts[1]);
+    protected void reduce(LongWritable key, Iterable<Text> texts, Context context) throws IOException, InterruptedException {
+        TreeMap<Long, String> ligne = new TreeMap<Long, String>();
+        /*Text (input qui nous vient du mapper), on re sélectionne selon le splitter de sortie du mapper ; */
+        for (Text text : texts) {
+            String[] parts = text.toString().split(";");
+            ligne.put(Long.valueOf(parts[0]), parts[1]);
         }
-        String rowString = StringUtils.join(row.values(), ' ');
-        context.write(key, new Text(rowString));
+        /*nouveau texte qui récupère les valeures récupérées et concatène*/
+        String text = StringUtils.join(ligne.values(), ' ');
+        /*cette nouvelle ligne a donc une clé (nulle car on ne veut pas afficher le numéro de ligne), et le texte formé*/
+        context.write(null, new Text(text));
     }
 }
  public static void main(String[] args) throws Exception {
