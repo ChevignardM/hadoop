9776ca1d5a7cf93eec537a5268f2815cdd2d3b95
Maxime Chevignard <maxime@chevignard.fr>
2017-10-18 21:45:04 +0200

    après test eclipse

--- tp1/src/Tp1.java
+++ tp1/src/Tp1.java
@@ -1,8 +1,6 @@
-import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapreduce.*;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
@@ -14,19 +12,20 @@ import java.util.*;
 import org.apache.hadoop.io.Text;
 
 public class Tp1{
-public static class TransposeMapper extends Mapper<LongWritable, Text, LongWritable, Text> {
+public static class PivotMap extends Mapper<LongWritable, Text, LongWritable, Text> {
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         long column = 0;
         long somethingLikeRow = key.get();
-        for (String num : value.toString().split(",")) {
+        /*réglage du splitter en semi-column*/
+        for (String num : value.toString().split(";")) {
             context.write(new LongWritable(column), new Text(somethingLikeRow + "\t" + num));
             ++column;
         }
     }
 }
 
-public static class TransposeReducer extends Reducer<LongWritable, Text, LongWritable, Text> {
+public static class PivotReduce extends Reducer<LongWritable, Text, LongWritable, Text> {
     @Override
     protected void reduce(LongWritable key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
         TreeMap<Long, String> row = new TreeMap<Long, String>();
@@ -35,14 +34,15 @@ public static class TransposeReducer extends Reducer<LongWritable, Text, LongWri
             row.put(Long.valueOf(parts[0]), parts[1]);
         }
         String rowString = StringUtils.join(row.values(), ' ');
-        context.write(new Text(rowString), NullWritable.get());
+        context.write(key, new Text(rowString));
     }
 }
  public static void main(String[] args) throws Exception {
     Configuration conf = new Configuration();
     Job job = Job.getInstance(conf, "Tp1");
     job.setJarByClass(Tp1.class);
-    job.setMapperClass(TransposeMapper.class);
+    job.setMapperClass(PivotMap.class);
+    job.setReducerClass(PivotReduce.class);
     job.setOutputKeyClass(LongWritable.class);
     job.setOutputValueClass(Text.class);
     FileInputFormat.addInputPath(job, new Path(args[0]));
